0803b2edff449de838f1dd92a603341cf2e186b2
Víctor Punzano <v.punzano@gmail.com>
2017-02-02 23:34:01 +0100

    Menu with toggle functionality completed.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,12 +1,18 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
   $(document).foundation();
 //   $('#offCanvasLeft').foundation('open');
   setEventHandlers();
 });
 
-function setEventHandlers(){
-  $('#off-canvas-toggle-button').click(function(){
-    $('#offCanvasLeft').toggleClass('reveal-for-large');
+function setEventHandlers () {
+  $('#off-canvas-content-toggle-button, #off-canvas-menu-toggle-button').click(function () {
+    $('#off-canvas-menu').toggleClass('reveal-for-large');
+    $('#off-canvas-content').toggleClass('menu-hidden');
+    $('#off-canvas-content-toggle-button').toggle();
   });
+
+//   $('#off-canvas-menu').click(function(event){
+//     event.stopPropagation();
+// });
 }
